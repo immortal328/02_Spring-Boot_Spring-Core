6bf2f51f7feb4b6c73a46f7933f7f24da6a8d931
Amar H. Pujari. <47078596+immortal328@users.noreply.github.com>
2024-05-26 02:26:01 +0530

    Bean lifeCycle methid

--- src/main/java/ap/immortal/comman/TrackCoach.java
+++ src/main/java/ap/immortal/comman/TrackCoach.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 import ap.immortal.interfaces.Coach;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 
 @Primary
 @Component
@@ -12,11 +14,22 @@ public class TrackCoach implements Coach {
 	TrackCoach(){
 		System.out.println("in constructor : "+ getClass().getSimpleName());
 	}
+	
+	@PostConstruct
+	void init(){
+		System.out.println("@PostConstruct : "+getClass().getSimpleName());
+
+	}
 
 	
 	@Override
 	public String getDailyWorkout() {
 		return "Run at least 20 minute with 10.5 Km/h speed..";
+	}
+	
+	@PreDestroy
+	void destroy() {
+		System.out.println("@PreDestroy : "+getClass().getSimpleName());
 	}
 
 }
